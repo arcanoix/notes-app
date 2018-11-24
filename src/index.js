a12e7edbd521f6248ebd2aabba73da19d33c83bc
Gustavo Herrera <gustavoh.2312@gmail.com>
2018-11-24 16:06:49 -0500

    module add, edit, index notes ready

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars');
 const methodo = require('method-override');
 const session = require('express-session');
 
-
+const flash = require('connect-flash');
 
 
 
@@ -34,10 +34,17 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+app.use(flash());
 
 
 // Globarl Variables
+app.use((req, res, next) => {
 
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+
+	next();
+});
 
 // Routes
 app.use(require('./routes/index'));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,9 +1,62 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/Note');
 
-router.get('/notes', (req, res) =>{
-	res.send('Notas traidas de la base de datos');
+router.get('/notes/add', (req, res) => {
+		res.render('notes/new-note');
+})
+
+router.post('/notes/new-note', async(req, res) => {
+
+		const {title, description} = req.body;
+		const errors = [];
+
+		if(!title) {
+			errors.push({text: 'Por favor Escriba un titulo'});
+		}
+		if(!description) {
+			errors.push({text: 'Por favor escriba una descripcion'});
+		}
+
+		if(errors.length > 0){
+			res.render('notes/new-note', {
+				errors,
+				title,
+				description
+			});
+		}else{
+			const newNote = new Note({title, description});
+			await newNote.save();
+			req.flash('success_msg','Nota agregada satisfactoriamente');
+			res.redirect('/notes');
+
+
+		}
+		
+});
+
+router.get('/notes', async(req, res) =>{
+	const notes = await Note.find().sort({date: 'desc'});
+	res.render('notes/all-notes', {notes});
+});
+
+router.get('/notes/edit/:id', async(req, res) => {
+	const note = await Note.findById(req.params.id);
+	res.render('notes/edit-note', {note});
+});
+
+router.put('/notes/edit-note/:id', async (req, res) =>{
+	const {title, description} = req.body;
+	await Note.findByIdAndUpdate(req.params.id, {title, description});
+	req.flash('success_msg','Se ha actualizado la nota');
+	res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async (req, res) =>{
+	await Note.findByIdAndDelete(req.params.id);
+	req.flash('success_msg','Se ha eliminado la nota');
+	res.redirect('/notes');
 });
 
 module.exports = router;
